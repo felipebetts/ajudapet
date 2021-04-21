ce6400ec39540a5b696f68f66c10bfe8a7afad96
Felipe Betts <72316797+felipebetts@users.noreply.github.com>
2021-04-21 18:22:47 -0300

    materialUi

--- pages/_app.js
+++ pages/_app.js
@@ -1,3 +1,4 @@
+import { useEffect } from "react";
 import { ThemeProvider, createGlobalStyle } from "styled-components"
 
 const theme = {
@@ -57,6 +58,15 @@ input::-webkit-inner-spin-button {
 // }
 
 function MyApp({ Component, pageProps }) {
+
+  useEffect(() => {
+    // Remove the server-side injected CSS.
+    const jssStyles = document.querySelector('#jss-server-side');
+    if (jssStyles) {
+      jssStyles.parentElement.removeChild(jssStyles);
+    }
+  }, []);
+
   return (
     <ThemeProvider theme={theme}>
       <GlobalStyle />
