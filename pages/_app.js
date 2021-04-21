1c26dbe65d92a7a407373e85ae93b68afbf5087a
Felipe Betts <72316797+felipebetts@users.noreply.github.com>
2021-04-21 18:32:13 -0300

    document

--- pages/_app.js
+++ pages/_app.js
@@ -59,13 +59,13 @@ input::-webkit-inner-spin-button {
 
 function MyApp({ Component, pageProps }) {
 
-  useEffect(() => {
-    // Remove the server-side injected CSS.
-    const jssStyles = document.querySelector('#jss-server-side');
-    if (jssStyles) {
-      jssStyles.parentElement.removeChild(jssStyles);
-    }
-  }, []);
+  // useEffect(() => {
+  //   // Remove the server-side injected CSS.
+  //   const jssStyles = document.querySelector('#jss-server-side');
+  //   if (jssStyles) {
+  //     jssStyles.parentElement.removeChild(jssStyles);
+  //   }
+  // }, []);
 
   return (
     <ThemeProvider theme={theme}>
